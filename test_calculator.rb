42570a7cd7d64f1ac00c8e1dbb2513888dc7b214
Rishabh Gupta <rishabh.works22@gmail.com>
2025-04-28 17:41:46 +0530

    Rename unit test names

--- test_calculator.rb
+++ test_calculator.rb
@@ -2,20 +2,19 @@ require 'test/unit'
 require_relative 'calculator'
 
 class CalculatorTest < Test::Unit::TestCase
-  def test_empty
+  def test_empty_string
     assert_equal 0, Calculator.add(''), "Adding empty string return 0"
   end
 
-  def test_single_number
+  def test_single_number_string
     assert_equal 22, Calculator.add('22'), "Passing single number string return itself"
   end
 
-  def test_non_string
+  def test_non_string_error
     assert_raise(ArgumentError, "Invalid input, expected string of comma / endline separated numbers") do
       Calculator.add(22)
     end
   end
-
 
   def test_comma_separated_addition
     assert_equal 15, Calculator.add('9,6'), 'Adding comma separated values'
