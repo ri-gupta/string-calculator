398af25d61cf4d2cdc24b5c5b0f30148d3303a25
Rishabh Gupta <rishabh.works22@gmail.com>
2025-04-28 18:47:29 +0530

    Refactor to use single split via regex expression

--- test_calculator.rb
+++ test_calculator.rb
@@ -2,18 +2,18 @@ require 'test/unit'
 require_relative 'calculator'
 
 class CalculatorTest < Test::Unit::TestCase
+  def test_non_string_error
+    assert_raise(ArgumentError, "Invalid input type #{22.class}, expected string of comma / endline separated numbers") do
+      Calculator.add(22)
+    end
+  end
+
   def test_empty_string
     assert_equal 0, Calculator.add(''), "Adding empty string return 0"
   end
 
   def test_single_number_string
     assert_equal 22, Calculator.add('22'), "Passing single number string return itself"
-  end
-
-  def test_non_string_error
-    assert_raise(ArgumentError, "Invalid input type #{22.class}, expected string of comma / endline separated numbers") do
-      Calculator.add(22)
-    end
   end
 
   def test_comma_separated_addition
@@ -27,5 +27,4 @@ class CalculatorTest < Test::Unit::TestCase
   def test_new_line_separated_additions
     assert_equal 26, Calculator.add('2\n7,9,\n8'), 'Adding comma separated values'
   end
-
 end
